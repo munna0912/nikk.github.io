1233bc350593feec7c89c70cb7d3ca44b0437d56
munna0912 <75537601+munna0912@users.noreply.github.com>
2021-01-21 23:07:03 +0530

    Add files via upload

---
+++ addication.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+ int main()
+ {float a,b,c;
+     printf("please enter first value\n");
+     scanf("%f" ,&a);
+     printf("enter second value\n");
+     scanf("%f" ,&b);
+     c=a+b;
+     printf("sum=%f\a",c);
+     getch();
+ }
